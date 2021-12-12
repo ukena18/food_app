3300ece96f422e04603c18b5ad52bee085b9d9ac
ukena18 <ukena18@gmail.com>
2021-12-11 23:05:55 -0700

    Added database conn

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--cksr3k*2+l3=(oc-tvmf+_#x(x(a^g+gg(-+%75%0342@m%1c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["ridwan-web.herokuapp.com/","127.0.0.1"]
 
