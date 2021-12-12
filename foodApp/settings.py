68f0259679495abcff8c05fa5dd5529512cf4f31
ukena18 <ukena18@gmail.com>
2021-12-11 22:42:53 -0700

    dAtabase changed

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
